c9f6827681abef12a9b7e0862a461c291d445c2d
Sydney Bruce <sydney@symbioticai.ca>
2024-10-21 12:15:59 -0600

    added nav and pages navigation with a tags

--- src/components/NavigationBar.svelte
+++ src/components/NavigationBar.svelte
@@ -1,11 +1,13 @@
 <nav class="w-full flex justify-between py-2 items-center border-black border-b-[2px]">
-  <div class="ml-4 items-center flex flex-col">
+  <div class="ml-4">
+    <a href="/" class="flex flex-col items-center justify-center">
     <img src="/icons/placeholder-icon.svg" alt="placeholder icon" class="h-[30px]"/>
     <p class="text-sm">Logo</p>
+    </a>
   </div>
   <ul class="flex mx-4">
-  <li class="mx-4 px-2">Design</li>
-  <li class="mx-4 px-2">Publishing</li>
-  <li class="mx-4 px-2">Profile</li>
+  <li class="mx-4 px-2"><a href="./design" class="hover:border-b-[1px] border-black">Design</a></li>
+  <li class="mx-4 px-2"><a href="./publishing" class="hover:border-b-[1px] border-black">Publishing</a></li>
+  <li class="mx-4 px-2"><a href="./profile" class="hover:border-b-[1px] border-black">Profile</a></li>
   </ul>
 </nav>
